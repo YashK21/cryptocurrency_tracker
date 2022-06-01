f6443ed1d92946149f44cbb0977a8b369664d620
yashk21 <yashk8119@gmail.com>
2022-06-01 17:44:34 +0530

    component argument in route tag replaced by element due to version issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-
+import Header from "./components/Header";
+import HomePage from "./Pages/HomePage";
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+        <Header />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Header = () => {
+  return <>Header</>;
+};
+export default Header;
